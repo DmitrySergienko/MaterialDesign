347c69b28dcb5371fb01859f3523231bd9956b07
DmitrySergienko <dmitrisergienko@gmail.com>
2022-03-08 22:46:09 +0300

    View Pager -> View Pager2

--- app/src/main/java/ru/ds/materialdesign/view/navigation/NavigationActivity.kt
+++ app/src/main/java/ru/ds/materialdesign/view/navigation/NavigationActivity.kt
@@ -3,6 +3,7 @@ package ru.ds.materialdesign.view.navigation
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import ru.ds.materialdesign.R
 import ru.ds.materialdesign.databinding.ActivityNavigationBinding
 
@@ -13,19 +14,32 @@ class NavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+        binding.viewPager.adapter = ViewPagerAdapter(this)
 
-       //связываем TabLayout с пейджером
-        binding.tabLayout.setupWithViewPager(binding.viewPager)
-
-        //binding.tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
-        //binding.tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_mars)
-        //binding.tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_system)
+        //связываем TabLayout с пейджером
+        TabLayoutMediator(binding.tabLayout,binding.viewPager
+        ) { tab, position ->
+            tab.text = when (position) {
+                EARTH_KEY -> "Earth"
+                MARS_KEY -> "Mars"
+                SYSTEM_KEY -> "System"
+                else -> "Earth"
+            }
+        }.attach()
 
         //устанавливаем кастомную иконку
         binding.tabLayout.getTabAt(0)?.setCustomView(R.layout.activity_navigation_tablayout_item_earth)
         binding.tabLayout.getTabAt(1)?.setCustomView(R.layout.activity_navigation_tablayout_item_mars)
         binding.tabLayout.getTabAt(2)?.setCustomView(R.layout.activity_navigation_tablayout_item_system)
+
+
+        //binding.tabLayout.setupWithViewPager(binding.viewPager)
+
+        //binding.tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
+        //binding.tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_mars)
+        //binding.tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_system)
+
+
 
 
     }
--- app/src/main/java/ru/ds/materialdesign/view/navigation/ViewPagerAdapter.kt
+++ app/src/main/java/ru/ds/materialdesign/view/navigation/ViewPagerAdapter.kt
@@ -1,29 +1,37 @@
 package ru.ds.materialdesign.view.navigation
 
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
 
-private const val EARTH_KEY = 0
-private const val MARS_KEY = 1
-private const val SYSTEM_KEY = 2
+    const val EARTH_KEY = 0
+    const val MARS_KEY = 1
+    const val SYSTEM_KEY = 2
 
-class ViewPagerAdapter(private val fragmentManager:FragmentManager): FragmentStatePagerAdapter(fragmentManager) {
+class ViewPagerAdapter(private val fragmentManager:FragmentActivity):
+        FragmentStateAdapter(fragmentManager) {
 
     //Хранилище фрагметов
     private val fragments = arrayOf(EarthFragment(), MarsFragment(), SystemFragment())
 
-    override fun getCount()= fragments.size // return quantity elements
-    override fun getItem(position: Int)= fragments[position] // return quantity elements
+    override fun getItemCount()= fragments.size // return quantity elements
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when(position){
-            EARTH_KEY -> "Earth"
-            MARS_KEY -> "Mars"
-            SYSTEM_KEY ->"System"
-            else -> "Earth"
-        }
-    }
+    override fun createFragment(position: Int)= fragments[position] // return quantity elements
+
+
+
+       // return when(position){
+       //     EARTH_KEY -> "Earth"
+       //     MARS_KEY -> "Mars"
+       //     SYSTEM_KEY ->"System"
+       //     else -> "Earth"
+       // }
+
+
+
 
 }
